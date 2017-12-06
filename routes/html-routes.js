978aec6cfa0e109c061748f388fe3c0a048c2a7f
Josh Loftis <jshlfts32@gmail.com>
2017-12-06 16:44:47 -0500

    Updates

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,6 +16,14 @@ router.get('/profile', (req, res, next) => {
   res.render('profile', { user: req.user });
 });
 
+router.get('/login', (req, res, next) => {
+  res.render('login', { user: req.user });
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup', { user: req.user });
+});
+
 router.get('/rewards', (req, res, next) => {
   res.render('rewards', { user: req.user });
 });
